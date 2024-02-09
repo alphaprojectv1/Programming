1f4be0e238e80f21e46794dced87c9a26c0fb040
Alpha <alphaprojectv1@gmail.com>
2024-02-10 01:25:40 +0600

    Union

--- Basic/ICA-10-5-Union-and-Union-By-Size.cpp
+++ Basic/ICA-10-5-Union-and-Union-By-Size.cpp
@@ -13,5 +13,24 @@ const int N=1e5+5;
 int par[N];
 void dsu_ini(int n)
 {
-
+   for(int i=0;i<n;i++)
+   {
+    par[i]=-1;
+   }
+}
+int dsu_find(int node)
+{
+    if(par[node]==-1)
+    {
+        return node;
+    }
+    int leader=dsu_find(par[node]);
+    return leader;
+}
+int main()
+{
+    int n;
+    cin>>n;
+    dsu_ini(n);
+    cout<<dsu_find(2);
 }
